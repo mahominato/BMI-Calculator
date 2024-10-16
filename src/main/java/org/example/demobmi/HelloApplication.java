ebb4729bab00aa05f5c12813c6bee4d19856763c
Cholpon <colponhalibaeva@gmail.com>
2024-10-16 21:54:06 +0600

    Inital commit

--- src/main/java/org/example/demobmi/HelloApplication.java
+++ src/main/java/org/example/demobmi/HelloApplication.java
@@ -8,11 +8,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 400, 500);
+        stage.setTitle("BMI Calculator");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/example/demobmi/HelloController.java
+++ src/main/java/org/example/demobmi/HelloController.java
@@ -1,14 +1,75 @@
 package org.example.demobmi;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.text.DecimalFormat;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField heightField1;
+    @FXML
+    private TextField weightField1;
+    @FXML
+    private Button calculateButton;
+    @FXML
+    private Label resultLabel;
+    @FXML
+    private Label bmiStatusLabel;
+    @FXML
+    private TextField heightField2;
+    @FXML
+    private TextField weightField2;
+    @FXML
+    private Button calculateButton1;
+    @FXML
+    private Label resultLabel1;
+    @FXML
+    private Label bmiStatusLabel1;
+
+    DecimalFormat df = new DecimalFormat("#.##");
+
+    @FXML
+    private void calculateMetricBMI() {
+        try {
+            double height = Double.parseDouble(heightField1.getText());
+            double weight = Double.parseDouble(weightField1.getText());
+            double bmi = weight / ((height / 100.0) * (height / 100.0));
+            resultLabel.setText(String.format("BMI: %s", df.format(bmi)));
+            bmiStatusLabel.setText(getBMIStatus(bmi));
+        } catch (NumberFormatException e) {
+            resultLabel.setText("Invalid input");
+            bmiStatusLabel.setText("");
+        }
+    }
+
+    @FXML
+    private void calculateEnglishBMI() {
+        try {
+            double height = Double.parseDouble(heightField2.getText());
+            double weight = Double.parseDouble(weightField2.getText());
+            double bmi = weight * 703.0 / (height * height);
+            resultLabel1.setText(String.format("BMI: %s", df.format(bmi)));
+            bmiStatusLabel1.setText(getBMIStatus(bmi));
+        } catch (NumberFormatException e) {
+            resultLabel1.setText("Invalid input");
+            bmiStatusLabel1.setText("");
+        }
+    }
+
+    private String getBMIStatus(double bmi) {
+        if (bmi < 18.5) {
+            return "Underweight";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            return "Normal";
+        } else if (bmi >= 25 && bmi < 29.9) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
     }
 }
